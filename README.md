9a7e9e52013f7f2a210c6d5806f01d6d51e3e6b8
Martin Petyrek <petyrekm@gmail.com>
2023-03-10 14:16:28 +0100

    update

--- README.md
+++ README.md
@@ -2,4 +2,10 @@
 
 ## generate models
 
+install swagger-codegen tool
+
+`brew install swagger-codegen`
+
+run the script
+
 `./scripts/generate-models.sh`
--- scripts/generate-models.sh
+++ scripts/generate-models.sh
@@ -8,7 +8,7 @@ set -e
 swagger-codegen generate -i http://localhost:8000/openapi.json -l typescript-axios -o ./tmp-swagger
 
 # Copy them over to generated models directory
-mv ./tmp-swagger/models/*.ts ./src/models/generated/
+mv ./tmp-swagger/models/*.ts ./src/data/_generated/
 
 # Remove the tmp contents
 rm -rf ./tmp-swagger
